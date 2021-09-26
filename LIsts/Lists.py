d917aa8c32b5e83f70f9f9d27de5b93168fd925f
Utkarsh-Raj20 <utkarshraj.code@gmail.com>
2021-09-26 11:29:58 +0530

    initial Commit2

---
+++ LIsts/Lists.py
@@ -0,0 +1,18 @@
+#1
+
+from typing import Type
+
+
+li = [5,3,6,2]
+
+li2 = ['hey', 5, 'yes', True]
+
+print(type(li2))
+
+familyage = [["Santosh Kumar", 45],
+            ["Pinki Kumari", 36],
+            ["Utkarsh Raj", 17],
+            ["Tejasb Raj", 11]]
+
+print(familyage)
+print(type(familyage))

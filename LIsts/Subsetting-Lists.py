1587cced9b9ae859564ef971e426161b728f6cbb
Utkarsh-Raj20 <utkarshraj.code@gmail.com>
2021-09-26 11:42:30 +0530

    Added Subsetting LIst

---
+++ LIsts/Subsetting-Lists.py
@@ -0,0 +1,19 @@
+
+family_age = [["Santosh Kumar", 45],
+            ["Pinki Kumari", 36],
+            ["Utkarsh Raj", 17],
+            ["Tejasb Raj", 11]]
+
+# SUBSETTING A LIST INCLUDE EXTRACTING A PART OF LIST
+#    X = familyage[ position ]
+#    X = familyage[ start : end ]
+#
+
+my_age = family_age[2]
+print(my_age)
+# ["Utkarsh Raj", 17]
+
+my_parents_age = family_age[0:2]
+print(my_parents_age)
+
+

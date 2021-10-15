c6b9fa366d9a4ca863b4385732465442631001df
Utkarsh-Raj20 <utkarshraj.code@gmail.com>
2021-10-15 09:05:13 +0530

    changes in function

--- Hakkerrank/function.py
+++ Hakkerrank/function.py
@@ -1,11 +1,7 @@
 n = int(input())
-ans = "1"
-if n == 1:
-    print(ans)
-else:   
-    i = 2
-    while i<=n:
-        ans = ans +' '+ str(i)
-        i+=1
-    print(ans)
-        
+ans = ""
+i = 1
+while i<=n:
+    ans = ans + str(i)
+    i+=1
+print(ans)

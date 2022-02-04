c94cb833a1d88ee87b2b27b4f680d3cc287de543
Utkarsh Raj <91374703+Utkarsh-Raj20@users.noreply.github.com>
2022-02-04 19:16:04 +0530

    Update GuessGame.py

--- Projects/Number Guessing Game/GuessGame.py
+++ Projects/Number Guessing Game/GuessGame.py
@@ -8,19 +8,23 @@ number = int(random.randint(lower, upper))
 tries = round(math.log(upper - lower + 1, 2))
 print("You have only ", tries, " tries to guess the number")
 guess = int(input("Guess a number:"))
-count = 0
+count = 1
+pas = True
 
-while count <= tries:
-    count += 1
+while count < tries:
     if guess == number:
         print("Congratulations, You guessed the number in ", count, " tries")
-        count = tries + 1
+        pas = False
+        break
     elif guess > number:
         print("Try Again, You guessed too high!")
         guess = int(input("Guess a number:"))
     else:
         print("Try Again, You guessed too small!")
         guess = int(input("Guess a number:"))
-if count > tries:
+    count += 1
+if guess == number and pas:
+    print("Congratulations, You guessed the number in ", count, " tries")
+elif count > tries:
     print("The number was ", number)
     print("Better luck nexy time")
